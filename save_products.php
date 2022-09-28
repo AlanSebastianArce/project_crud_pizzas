b3b24ed6af81ba6c64ae8f31a4249c600acb5fb6
Alan Sebastian Arce (CFP 401) <alanarce.fceyn.uba@gmail.com>
2022-09-28 19:47:15 -0300

    upload_image.php-Dia 1

--- save_products.php
+++ save_products.php
@@ -9,8 +9,22 @@ include_once("config_products.php");
 include_once("db.php");
 $link = new Db();
 include_once('upload_image.php');
-$path_img=$directorio.$nombre_img;   //Ruta completa de la imagen levantada.
-$sql="insert products(product_name, image, price, id_category) values (:product,:path_img,:price,:category)";
+$path_img = $directorio . $nombre_img;   //Ruta completa de la imagen levantada.
+$sql = "insert products(product_name, image, price, id_category) values (:product,:path_img,:price,:category)";
 $stmt = $link->prepare($sql);
-$stmt->execute();
-$data = $stmt->fetchAll();
+$stmt->bindValue(':product', $product);
+$stmt->bindValue(':price', $price);
+$stmt->bindValue(':category', $category);
+$stmt->bindValue(':path_img', $path_img);  //No es necesario sanear
+
+if($stmt->execute()){
+    ?>
+    <script>
+    alert("Producto Ingresado!");
+    window.location="insert_products.php";
+    </script>
+    <?php
+}
+
+//$stmt->execute();
+//$data = $stmt->fetchAll();
