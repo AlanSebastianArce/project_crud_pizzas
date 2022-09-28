f8c0b736ad4b79ce741c4560d5e8361153edbcb2
Alan Sebastian Arce (CFP 401) <alanarce.fceyn.uba@gmail.com>
2022-09-28 19:23:02 -0300

    save_products-2do Dia

--- save_products.php
+++ save_products.php
@@ -1,6 +1,16 @@
 <?php
-$product=$_POST['producto'];
-$price=$_POST['precio'];
-$category=$_POST['categoria'];
+$product = $_POST['producto'];
+$price = $_POST['precio'];
+$category = $_POST['categoria'];
 
-echo $product." ".$price." ".$category;
+/** echo $product." ".$price." ".$category; **/
+
+include_once("config_products.php");
+include_once("db.php");
+$link = new Db();
+include_once('upload_image.php');
+$path_img=$directorio.$nombre_img;   //Ruta completa de la imagen levantada.
+$sql="insert products(product_name, image, price, id_category) values (:product,:path_img,:price,:category)";
+$stmt = $link->prepare($sql);
+$stmt->execute();
+$data = $stmt->fetchAll();
